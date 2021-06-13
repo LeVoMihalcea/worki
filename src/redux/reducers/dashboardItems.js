c9455840840fd48e5d99c7c90f0755c1a9087df4
LeonardM <leomihalcea@gmail.com>
2021-06-14 00:20:35 +0300

    [#16][#17][#18][#19] Generalize Items in order to start procedural generation of them

--- src/redux/reducers/dashboardItems.js
+++ src/redux/reducers/dashboardItems.js
@@ -4,11 +4,11 @@ export const dashboardItemsSlice = createSlice({
     name: 'items',
     initialState: {
         items: {
-            0: {type: "counter", icon: "coffee.svg", title: "Cups of coffee", counter: 0},
-            1: {type: "counter", icon: "drink-water.svg", title: "Cups of Water", counter: 0},
-            3: {type: "timer", title: "Straighten your Back Reminder", sound: "juntos.mp3", min: 1, max: 30, step: 5, minutes: 5},
+            4: {type: "counter", icon: "coffee.svg", title: "Cups of coffee", counter: 0},
+            3: {type: "counter", icon: "drink-water.svg", title: "Cups of Water", counter: 0},
+            0: {type: "timer", title: "Straighten your Back Reminder", sound: "juntos.mp3", min: 1, max: 30, step: 5, minutes: 5},
             2: {type: "counter", icon: "coffee.svg", title: "Cups of coffee", counter: 0},
-            4: {type: "timer", title: "Take a well-deserved break!", sound: "juntos.mp3", min: 1, max: 30, step: 5, minutes: 5},
+            1: {type: "timer", title: "Take a well-deserved break!", sound: "juntos.mp3", min: 30, max: 60, step: 10, minutes: 50},
         },
         dialogOpen: false,
     },
